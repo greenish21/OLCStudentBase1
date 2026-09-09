584ef5fd18c1391cb7b6b0e35908a8a6f7d6afcc
greenish21 <zxngei2@gmail.com>
2026-09-09 13:22:02 +0000

    9/10/26

--- 2024.py
+++ 2024.py
@@ -10,6 +10,29 @@ def odd_or_even(number):
         return "Odd"
 # Q11
 def prime(number):
-    if odd_or_even(number) == "Odd" or number == 2:
+    half_num = div_2(number)
+    count = 3
+    if number == 2:
+        return "prime"
+    elif number < 2:
+        return "not prime"
+    elif odd_or_even(number) == "Odd":
+        while count <= half_num:
+            if number % count == 0:
+                return "not prime"
+            count += 1
+        return "prime"
+    else:
+        return "not prime"
+
+while True:
+    number = int(input("Enter a whole number: "))
+    if type(number) == int:
+        break
+    else:
+        print("Enter a whole number only.")
+print(f"{number} is {prime(number)}.")
+
+
         
 
